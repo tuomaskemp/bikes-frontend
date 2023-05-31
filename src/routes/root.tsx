cb68ce4b85a8282495ae8e76b4bdb72a7f588433
Tuomas Kemppainen <tuomas.kemppainen@student.hamk.fi>
2023-05-31 17:05:52 +0300

    added covered distance and duration to journey listing

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -3,10 +3,28 @@ import { Journey } from "../types"
 
 export default function Root() {
     const journeys = useLoaderData() as Journey[]
+    const secondsToMinutes = (seconds: number) => {
+        const date = new Date();
+        date.setSeconds(seconds);
+        const minutes = date.getMinutes();
+        return <>{minutes} minutes</>;
+    }
+    const metersToKilometers = (meters: number) => {
+        if (meters < 1) {
+            return;
+        } else {
+            return meters / 1000;
+        }
+    }
   return (
     <>
     {journeys.map(journey => (
-        <p key={journey.id}>Departure: {journey.departureStation.name} Return: {journey.returnStation.name}</p>
+        <div key={journey.id} className="mb-4">
+            <p>Departure: {journey.departureStation.name}</p>
+            <p>Return: {journey.returnStation.name}</p>
+            <p>Covered distance: {metersToKilometers(journey.coveredDistanceInMeters)} kilometers</p>
+            <p>Duration: {secondsToMinutes(journey.durationInSeconds)}</p>
+        </div>
     ))}
     </>
   )
